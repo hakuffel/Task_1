bdd213dbd2e829832d2856e4de3a5c26cdfff4ef
Ekaterina@Hakuffel <katebutovo@mail.ru>
2026-07-20 03:08:01 +0300

    Добавлены проверки на методы move и get_price, захардокжено в def test_get_receipt_contains_price

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,6 +1,7 @@
 import pytest
 
 from praktikum.burger import Burger
+from data import BUN_NAME_1, BUN_PRICE_1, SAUCE_NAME, SAUCE_PRICE, FILLING_NAME, FILLING_PRICE
 
 
 class TestBurger:
@@ -31,7 +32,15 @@ class TestBurger:
 
     def test_get_receipt_contains_price(self, burger_with_ingredients):
         receipt = burger_with_ingredients.get_receipt()
-        assert f'Price: {burger_with_ingredients.get_price()}' in receipt
+        expected_receipt = (
+            f'(==== {BUN_NAME_1} ====)\n'
+            f'= sauce {SAUCE_NAME} =\n'
+            f'= filling {FILLING_NAME} =\n'
+            f'(==== {BUN_NAME_1} ====)\n'
+            '\n'
+            f'Price: {BUN_PRICE_1 * 2 + SAUCE_PRICE + FILLING_PRICE}'
+        )
+        assert receipt == expected_receipt
 
     @pytest.mark.parametrize('ingredient_fixture', ['mock_sauce', 'mock_filling'])
     def test_get_receipt_contains_ingredient_name(self, request, burger_with_bun, ingredient_fixture):
@@ -45,3 +54,11 @@ class TestBurger:
         sauce_position = receipt.find('hot sauce')
         filling_position = receipt.find('cutlet')
         assert sauce_position < filling_position
+
+    def test_move_ingredient_changes_order(self, burger_with_ingredients, mock_sauce, mock_filling):
+        burger_with_ingredients.move_ingredient(0, 1)
+        assert burger_with_ingredients.ingredients == [mock_filling, mock_sauce]
+
+    def test_get_price_returns_correct_total(self, burger_with_ingredients):
+        expected_price = BUN_PRICE_1 * 2 + SAUCE_PRICE + FILLING_PRICE
+        assert burger_with_ingredients.get_price() == expected_price
